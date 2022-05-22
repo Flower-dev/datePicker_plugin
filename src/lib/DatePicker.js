7996edb3cf221c98eea9dc261671f7ae8d99c839
Flower Dev <67862441+Flower-dev@users.noreply.github.com>
2022-05-22 19:00:30 +0200

    propsTypes + custom

--- src/lib/DatePicker.js
+++ src/lib/DatePicker.js
@@ -1,8 +1,20 @@
 import React, {useState, useEffect, createRef } from 'react';
+import PropsTypes from 'prop-types';
 import './datePicker.scss';
 
 
-export function DatePicker ({onChange}) {
+DatePicker.PropsTypes = {
+    onChange: PropsTypes.func,
+    disabled: PropsTypes.bool,
+    required: PropsTypes.bool
+};
+
+DatePicker.DefaultProps = {
+    disabled: false,
+    required: false
+}
+
+export function DatePicker ({onChange, disabled, required}) {
     let oneDay = 60 * 60 * 24 * 1000;
     let todayTimestamp = Date.now() - (Date.now() % oneDay) + (new Date().getTimezoneOffset() * 1000 * 60);
     let date = new Date();
@@ -219,6 +231,8 @@ export function DatePicker ({onChange}) {
                 <input 
                     type='date' 
                     ref={inputRef}
+                    required={required}
+                    disabled={disabled}
                     onChange={updateDateFromInput}
                 />
             </div>
